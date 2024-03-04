0c823811b80cd5fa9ea4931655ea27296d3693a4
GavinStrikes <105064040+GavinCoded@users.noreply.github.com>
2024-03-04 16:55:44 -0500

    test

---
+++ wtf/oblivity/credentials.php
@@ -0,0 +1,5 @@
+<?php
+$name = "nigger"; // Application name
+$OwnerId = "EdmsTKiuld"; // Application ownerID
+$SellerKey = "f07244b2689571fc12e10541ed6aab07"; // application's seller key. Found in seller settings (You must have seller plan)
+?>
